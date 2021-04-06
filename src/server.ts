b1153f82f074fb1f9fb4f3621ccf2a4dafa4c873
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-04-05 22:40:52 -0300

    Install ts-node-dev as development dependency to not need to convert ts into js

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from "express";
 
-const server = express();
+const app = express();
 
-const test = "asd";
+app.get("/", (request, response) => response.json({ message: "HIIHIHIHIHI" }));
 
-server.get("/", (request, response) =>
-  response.json({ message: "HIIHIHIHIHI" })
-);
-
-server.listen(3333);
+app.listen(3333, () => {
+  console.log("Server is running");
+});
