30375d9a92014949184bf6bc2f188b971d30da72
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-04-06 22:58:33 -0300

    Add a VSCode debug setup

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) =>
   response.json({ message: "Hello World!!!!!!!!!" })
 );
+
+app.post("/test", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
 
 app.listen(3333, () => {
   console.log("Server is running");
