1539aae53792a8dffa95e734c8381f64fde28f9a
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-04-28 22:56:56 -0300

    Add use case for listing categories

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,12 @@
+import { Category } from "../../model/Category";
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute(): Category[] {
+    return this.categoriesRepository.list();
+  }
+}
+
+export { ListCategoriesUseCase };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-
-const repository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  return response.json(repository.list());
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
